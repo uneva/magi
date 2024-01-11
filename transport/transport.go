0cef6fa1c8dfa32cecb02d0f646d2ecb460bdac6
maggot_code <63178315+maggot-code@users.noreply.github.com>
2024-01-11 23:44:28 +0800

    Update dependencies in go.mod file

--- transport/transport.go
+++ transport/transport.go
@@ -1,15 +1,1 @@
 package transport
-
-import "github.com/gin-gonic/gin"
-
-type TransportHandle func(gin.Context) (interface{}, error)
-
-type Transport struct {
-	engine *gin.Engine
-}
-
-func New(e *gin.Engine) *Transport {
-	return &Transport{
-		engine: e,
-	}
-}
